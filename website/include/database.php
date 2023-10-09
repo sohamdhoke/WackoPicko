844933a4859cbf659ea88f5cea258d0597e8004b
Soham Dhoke <94436002+sohamdhoke@users.noreply.github.com>
2023-10-09 12:11:07 +0530

    Update database.php

--- website/include/database.php
+++ website/include/database.php
@@ -5,7 +5,7 @@ class DB
    
    function __construct($server, $user_name, $pass, $db)
    {
-      $this->conn = mysql_connect($server, $user_name, $pass);
+      $this->conn = mysqli_connect($server, $user_name, $pass);
       mysql_select_db($db, $this->conn);
       mysql_set_charset('utf8');
    }
